009e43a6d4aab075c9a7506de88bcebfa3279297
YOU ZHE <77982483+youzhe0305@users.noreply.github.com>
2024-05-15 20:11:30 +0800

    Add comment in main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get('PORT', default=8000))
+    # activate debug mode when developer mode is on
     debug = True if os.environ.get(
         'API_ENV', default='develop') == 'develop' else False
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
